89487dbfc79b724f765b1635b2c03692bbb22aa6
Kirillm <zoxynomorf@gmail.com>
2021-06-04 13:44:17 +0300

    обнавлен роут городов

--- routes/city.js
+++ routes/city.js
@@ -4,5 +4,6 @@ const { City } = require('../controllers')
 router.route('/').get(City.getAll)
 router.route('/:id').get(City.getOne)
 router.route('/').post(City.create)
+router.route('/:id').delete(City.delete)
 
 module.exports = router
